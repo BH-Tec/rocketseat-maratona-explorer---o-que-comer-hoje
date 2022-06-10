733dec4de0b3eabe5f9461736c86285a018422dc
Bruno Henrique Félix Dórea <brunodorea@outlook.com.br>
2022-06-10 15:26:51 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const elementoResposta = document.querySelector('#resposta');
+const buttonPerguntar = document.querySelector('#buttonPerguntar');
+const respostas = [
+  "pizza",
+  "crepioca",
+  "pão com ovo frito"
+]
+
+// clicar em fazer pergunta
+function fazerPergunta() {
+
+  // desabilita o botão
+  buttonPerguntar.setAttribute('disabled', true);
+
+
+  // gerar um número aleatório
+  const totalRespostas = respostas.length;
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas);
+
+  // exibir a resposta
+  elementoResposta.innerHTML = respostas[numeroAleatorio];
+  
+  elementoResposta.style.opacity = 1;
+
+  // sumir a resposta apos 3 segundos
+  setTimeout(function() {
+    elementoResposta.style.opacity = 0;
+  }, 7000);
+}
